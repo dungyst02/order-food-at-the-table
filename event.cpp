ace2b28bc9057d89fe347b286228e485f79d53b0
root <root@LAPTOP-DUNGNGUYEN>
2023-04-11 21:32:57 +0700

    updated event.cpp

--- event.cpp
+++ event.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<ctime>
 #include<random>
+#include<chrono>
+#include <thread>
+
 struct Order{
     int tableNumber;  //so thu tu ban
     long orderNumber; // so order
@@ -11,7 +14,7 @@ struct Button{
     std::time_t buttonTime; // thoi gian bam nut
 };
 
-void randomOrder( Order& order) {
+void randomOrder(Order& order) {
     std::random_device rd; // tạo seed ngẫu nhiên
     std::mt19937 eng(rd()); // tạo engine
     std::uniform_int_distribution<> tableDistr(1, 100); // tạo phân phối đều trong khoảng từ 1 đến 100
@@ -31,18 +34,60 @@ void randomButton(Button& button) {
     button.buttonNumber = buttonDistr(eng);
     button.buttonTime = timeDistr(eng); // sinh giá trị thời gian ngẫu nhiên trong khoảng từ 1 giờ trước đến thời điểm hiện tại
 }
+
+//Ham xu ly cac event
+bool handlingEvents(Order* order, Button* button){    
+    if(order == nullptr || button == nullptr) {
+        return false;
+    }
+    if(order->tableNumber != button->buttonNumber) {
+        return false;
+    }
+    // Tính toán độ lệch thời gian giữa hai thời điểm
+    auto orderTimePoint = std::chrono::system_clock::from_time_t(order->orderTime);
+    auto buttonTimePoint = std::chrono::system_clock::from_time_t(button->buttonTime);
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(buttonTimePoint - orderTimePoint);
+
+    if(duration.count() >= 60) {
+        return false;
+    }
+    return true;
+}
+//Hiển thị các order thành công 
+void display() {
+    std::vector<Order> orders;
+    std::vector<Button> buttons;
+    while (true) {
+        // Sinh order và button ngẫu nhiên
+        Order order;
+        randomOrder(order);
+        Button button;
+        randomButton(button);
+
+        // Kiểm tra xem order và button có thành công hay không
+        if (handlingEvents(&order, &button)) {
+            orders.push_back(order);
+            buttons.push_back(button);
+        }
+
+        // In danh sách các order và button thành công
+        system("clear"); // Xóa màn hình console trên Linux
+        std::cout << "Successful orders:\n";
+        for (size_t i = 0; i < orders.size(); i++) {
+            std::cout << "Table number: " << orders[i].tableNumber << ", ";
+            std::cout << "Order number: " << orders[i].orderNumber << ", ";
+            std::cout << "Order time: " << std::asctime(std::localtime(&orders[i].orderTime));
+        }
+        std::cout << "\nSuccessful buttons:\n";
+        for (size_t i = 0; i < buttons.size(); i++) {
+            std::cout << "Button number: " << buttons[i].buttonNumber << ", ";
+            std::cout << "Button time: " << std::asctime(std::localtime(&buttons[i].buttonTime));
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(5)); // Tạm dừng 1s
+    }
+}
+
 int main() {
-    Order order;
-    randomOrder(order);
-    Button button;
-    randomButton(button);
-      // In ket qua
-    std::cout << "Random Order:\n";
-    std::cout << "Table number: " << order.tableNumber << "\n";
-    std::cout << "Order number: " << order.orderNumber << "\n";
-    std::cout << "Order time: " << std::asctime(std::localtime(&order.orderTime));
-    std::cout << "\nRandom Button:\n";
-    std::cout << "Button number: " << button.buttonNumber << "\n";
-    std::cout << "Button time: " << std::asctime(std::localtime(&button.buttonTime));
+    display();
     return 0;
 }
